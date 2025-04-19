675606b4c49314c2afdcf9e504b1f0f89e087ed7
kamosah <68175563+kamosah@users.noreply.github.com>
2025-04-19 03:12:49 -0400

    chore: Add Styled Components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+
 import "./index.css";
 import App from "./App.tsx";
+import { DARK_THEME } from "./styles";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={DARK_THEME}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
