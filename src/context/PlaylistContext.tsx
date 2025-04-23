b0437bbffb78a26a043bdad750ed3e4e4525337d
kamosah <68175563+kamosah@users.noreply.github.com>
2025-04-22 22:26:07 -0400

    chore: remove dead code

--- src/context/PlaylistContext.tsx
+++ src/context/PlaylistContext.tsx
@@ -1,21 +1,13 @@
-import React, { createContext } from "react";
+import { createContext } from "react";
 import { Playlist } from "../types";
 import DATA from "../data/playlists.json";
 
-type DispatchFunction<T> = React.Dispatch<React.SetStateAction<T>>;
-
 export type PlaylistContextValue = {
-  playlistID: number;
-  currentTrackIndex: number;
   playlists: Playlist[];
-  setPlaylistID: DispatchFunction<number>;
-  setCurrentTrackIndex: DispatchFunction<number>;
 };
 
 export const DEFAULT_PLAYLIST_STATE = {
-  playlistID: 1,
   playlists: DATA.playlists,
-  currentTrackIndex: 0,
 } as const;
 
 // Create the context with an empty default value
--- src/context/PlaylistContextProvider.tsx
+++ src/context/PlaylistContextProvider.tsx
@@ -4,25 +4,11 @@ import { DEFAULT_PLAYLIST_STATE, PlaylistContext } from "./PlaylistContext";
 export const PlaylistContextProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  const [playlistID, setPlaylistID] = useState<number>(
-    DEFAULT_PLAYLIST_STATE.playlistID
-  );
-  const [currentTrackIndex, setCurrentTrackIndex] = useState<number>(
-    DEFAULT_PLAYLIST_STATE.currentTrackIndex
-  );
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [playlists, _] = useState(DEFAULT_PLAYLIST_STATE.playlists);
 
   return (
-    <PlaylistContext.Provider
-      value={{
-        playlistID,
-        playlists,
-        setPlaylistID,
-        setCurrentTrackIndex,
-        currentTrackIndex,
-      }}
-    >
+    <PlaylistContext.Provider value={{ playlists }}>
       {children}
     </PlaylistContext.Provider>
   );
